f879c7df9ca926e9bc15029f5eec165b020e274a
LittleYe233 <littleye233@foxmail.com>
2021-08-20 21:08:02 +0800

    Edit "run.py" to add an argument parser
    Use `argparse` module to add an argument parser. Parameters `port`,
    `host` and `debug` are set up.

--- run.py
+++ run.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python3
+
 from app import app
 from app import db
 from random import randint
 from app.models import Review, Course, Student, User
-import sys
+import argparse
 
-debug = False
-for arg in sys.argv:
-    if arg == '-d':
-        debug = True
 
+VERSION = 'ustc-course (forked) v0.1.0'  # should follow Semantic Versioning
+
+
+# parse the arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--debug', help='run the server in debug mode',
+                    action='store_true')
+parser.add_argument('-H', '--host', type=str, default='0.0.0.0',
+                    help='set host of the server (default value: 0.0.0.0)')
+parser.add_argument('-p', '--port', type=int, default=8080,
+                    help='set port of the server (default value: 8080)')
+parser.add_argument('-v', '--version', help='print the version string',
+                    action='store_true')
+args = parser.parse_args()
+
+if args.version:
+    print(VERSION)
+    exit(0)
+
+
+# set up the server
 def start():
-    if debug:
+    if args.debug:
         db.create_all()
-        # default process = 4, raising ValueError
-        app.run(port=8080, processes=True, host='0.0.0.0')
-    else:
-        app.run(port=3000, processes=True)
+    # default `processes` is 4, raising ValueError
+    app.run(port=args.port, processes=True, host=args.host)
 
 
-
+# Main function
 if __name__ == '__main__':
     start()
